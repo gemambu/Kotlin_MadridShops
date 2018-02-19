95c7217a729186aab11f5cf3c970e09610aa58dc
Gema Martinez <gemambu@gmail.com>
2018-02-19 08:56:13 +0100

    personalized pin map

--- app/src/main/java/com/gmb/madridshops/util/map/MapUtil.kt
+++ app/src/main/java/com/gmb/madridshops/util/map/MapUtil.kt
@@ -1,10 +1,11 @@
 package com.gmb.madridshops.util.map
 
 import android.content.Context
+import com.gmb.madridshops.R
 import com.gmb.madridshops.domain.model.Entity
-import com.gmb.madridshops.domain.model.Shop
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -31,7 +32,8 @@ class MapUtil {
 
         for (pinnable in mapPinnables) {
             val position = LatLng(pinnable.latitude.toDouble(), pinnable.longitude.toDouble())
-            val marker = MarkerOptions().position(position).title(pinnable.pinDescription)
+            val icon = BitmapDescriptorFactory.fromResource(R.drawable.pin)
+            val marker = MarkerOptions().position(position).title(pinnable.pinDescription).icon(icon)
             val m = googleMap.addMarker(marker)
             m.tag = pinnable.relatedModelObject
         }
