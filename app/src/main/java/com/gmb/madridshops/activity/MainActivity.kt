f0863b9aff63f525551abdd34350a68e9d070f21
Gema Martinez <gemambu@gmail.com>
2018-02-21 22:00:48 +0100

    added progress bar. DANGER! buttons disabled on mainActivity

--- app/src/main/java/com/gmb/madridshops/activity/MainActivity.kt
+++ app/src/main/java/com/gmb/madridshops/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gmb.madridshops.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import com.gmb.madridshops.R
 import com.gmb.madridshops.router.Router
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        disableButtons()
+        displaySpinner()
 
         main_activity_shops_button.setOnClickListener {
             Log.d(MainActivity::class.java.canonicalName, "Clicked on Shops")
@@ -23,6 +28,29 @@ class MainActivity : AppCompatActivity() {
             Router().navigateFromMainActivityToActivitiesActivity(this)
         }
 
+    }
+
+    fun displaySpinner(){
+
+        main_activity_progress_bar.visibility = View.VISIBLE
+        main_activity_progress_bar.animate()
+
+    }
+
+    fun hideSpinner(){
+
+        main_activity_progress_bar.visibility = View.GONE
+
+    }
+
+    fun disableButtons(){
+        main_activity_activities_button.isEnabled = false
+        main_activity_shops_button.isEnabled = false
+    }
+
+    fun enableButtons(){
+        main_activity_activities_button.isEnabled = true
+        main_activity_shops_button.isEnabled = true
     }
 
 }
